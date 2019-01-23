7e6810bfed79ba52da35d5066ce780c5d17c1383
Zamiz <zamiztobar@gmail.com>
2019-01-22 21:43:53 -0300

    feat(routes): add member routes to projects and people

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  resources :projects
-  resources :people
+  resources :projects do
+    member do
+      post 'add_people'
+      delete 'remove_people/:person_id', to: 'project#remove_person', as: 'remove_person'
+  end
+  
+  resources :people do
+    member do
+      post 'add_project'
+      delete 'remove_project/:project_id', to: 'people#remove_project', as: 'remove_project' 
+      end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
